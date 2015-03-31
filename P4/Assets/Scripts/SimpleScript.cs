92478845d7f96d7a40a80627a227878c3a5c6703
DDFranky22 <mannala@macrowebmedia.it>
2015-03-31 23:12:48 +0200

    added the simple script to test purpose

---
+++ P4/Assets/Scripts/SimpleScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("Hi, this script was created only to test git. If you are going to judgje me: shut up, take a cactus and fuckyouself. Thank you");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
