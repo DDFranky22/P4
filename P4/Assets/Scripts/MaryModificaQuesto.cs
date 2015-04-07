e683da2c4771eb6e4a0846f287f27d1c9ca31b54
DDFranky22 <mannala@macrowebmedia.it>
2015-04-07 18:54:42 +0200

    Script di test per Mary

---
+++ P4/Assets/Scripts/MaryModificaQuesto.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MaryModificaQuesto : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("modifica questa stringa di testo");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
